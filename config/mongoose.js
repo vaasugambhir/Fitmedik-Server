be890f5be820fdd21ba8b23c7a4de3e453c80a8f
Prateek Goyal <gprateek015@gmail.com>
2022-11-15 23:53:21 +0530

    hide mongo url

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 export default async function ConfigureDatabase() {
   await mongoose
-    .connect(
-      'mongodb+srv://fitmedik:fitmedik@cluster0.kg9dsge.mongodb.net/?retryWrites=true&w=majority',
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     .then(() => console.log('Connected to the database successfully.'))
     .catch(() => console.log('Error in connecting to the database.'));
 }

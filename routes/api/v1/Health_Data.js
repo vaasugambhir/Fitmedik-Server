a10ec05e32b3275415efc85e2e175c782e60028d
Prateek Goyal <gprateek015@gmail.com>
2022-12-21 23:47:58 +0530

    added routes for health data

--- routes/api/v1/Health_Data.js
+++ routes/api/v1/Health_Data.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { getWeekshealthData } from '../../../controllers/api/v1/Health_Data.js';
+import {
+  getWeekshealthData,
+  workLife,
+  storeDailyStepCount
+} from '../../../controllers/api/v1/Health_Data.js';
 
 const router = Router();
 
 router.get('/get-weeks-health-data', getWeekshealthData);
+router.post('/work-life', workLife);
+router.post('/daily-steps', storeDailyStepCount);
 
 export default router;

07b3f07c45ae21d9dfc1d91734e046a97d63eab4
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-01-22 20:47:16 +0530

    otp bug fixed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { Home } from '../controllers/Home_Controller.js';
-import { SignUp, Login, message,otpTest,CheckToken } from '../controllers/SignUpController.js';
+import { SignUp, Login, message,otpTest,CheckToken,CheckUser} from '../controllers/SignUpController.js';
 import apiRoutes from './api/index.js';
+
 
 const router = Router();
 
@@ -11,6 +12,9 @@ router.post('/login', Login);
 router.get('/mess', message);
 router.get('/', Home);
 router.post("/otp",otpTest);
-router.post("/checkOtp",CheckToken)
+router.post("/checkOtp",CheckToken);
+router.post("/check",CheckUser);
+
+// router.post('/NewOtp',NewOtp);
 
 export default router;

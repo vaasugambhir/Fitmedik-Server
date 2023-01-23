951d21ac4cabfb8affa107181fbcb3442ad72445
Aaditya-23 <aaditya220055@gmail.com>
2023-01-23 20:30:07 +0530

    fake users

--- controllers/api/v1/Api_Home.js
+++ controllers/api/v1/Api_Home.js
@@ -1,6 +1,5 @@
 export const apiHome = async (req, res) => {
   try {
-    console.log('asjkha');
     return res.status(200).json({ message: "Welcome to Fitmedik's API" });
   } catch (error) {
     console.log(error.message);
